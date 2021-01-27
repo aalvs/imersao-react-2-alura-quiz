e00e2d8077f235ae2bb0c0216b74b2361e56c519
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-26 23:05:02 -0300

    Ajustado identação do código

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -7,17 +10,23 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Patrick+Hand&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import styled from 'styled-components'
-import Head from 'next/head'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
 
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
